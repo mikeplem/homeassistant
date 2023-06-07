5ef0cb56929d0f4c7ec9a95938f4e3d07a4ab5a2
Mike Plemmons <mikeplemmons@gmail.com>
2023-06-07 19:53:58 -0400

    Forgot to quote the renew command

--- renew-ssl.sh
+++ renew-ssl.sh
@@ -1,7 +1,7 @@
 /root/acme.sh/acme.sh --renew-all --dns dns_aws \
 --server https://acme-v02.api.letsencrypt.org/directory \
 --keylength ec-384 \
---renew-hook systemctl restart nginx \
+--renew-hook "systemctl restart nginx" \
 -d homeassistant.n1mtp.com \
 -d hasswg.n1mtp.com
 
