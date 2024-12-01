81d05d3ce6456d1704b3348fbaf1eaa1e5990bbf
Mike Plemmons <mikeplemmons@gmail.com>
2024-12-01 08:32:35 -0500

    support TWC like container

--- get-ssl.sh
+++ get-ssl.sh
@@ -1,4 +1,4 @@
-/root/acme.sh/acme.sh --renew-all --dns dns_aws \
+/root/acme.sh/acme.sh --issue --dns dns_aws \
 --server https://acme-v02.api.letsencrypt.org/directory \
 --keylength ec-384 \
 --renew-hook "systemctl restart nginx" \
